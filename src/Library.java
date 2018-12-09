97b7e67b9f337fdb66faadb2748544419ce6bc65
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2018-12-10 00:35:14 +0100

    book1

---
+++ src/Library.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Library {
+    public static void main(String[] args) {
+        ArrayList <Book> books = new ArrayList<Book>();
+        books.add(new Book("O psie który jeździł koleją","Roman Pisarski",83234));
+        books.add(new Book("Lalka","Bolesław Prus", 121212));
+
+        for(Book b:books){
+            System.out.println("Title: " + b.getTitle()+ " ,author: " +b.getAuthor()+ " ,ISBN: " +b.getIsbn());
+
+        }
+
+    }
+
+}
